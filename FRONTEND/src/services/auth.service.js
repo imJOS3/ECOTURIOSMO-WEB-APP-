310d5f47ce07c5cc3e328aba94513437a2551937
SANTONXD <santiago1210molina@gmail.com>
2026-05-15 17:39:24 -0500

    Delete FRONTEND directory

--- FRONTEND/src/services/auth.service.js
+++
@@ -1,8 +0,0 @@
-import api from './api'
-
-export const authService = {
-  register: (data)  => api.post('/auth/register', data),
-  login:    (data)  => api.post('/auth/login', data),
-  logout:   ()      => api.post('/auth/logout'),
-  me:       ()      => api.get('/auth/me'),
-}
--- FRONTEND/src/services/resenas.service.js
+++
@@ -1,6 +0,0 @@
-import api from './api'
-
-export const resenasService = {
-  getByAlojamiento: (id)   => api.get(`/resenas/${id}`),
-  crear:            (data) => api.post('/resenas', data),
-}
